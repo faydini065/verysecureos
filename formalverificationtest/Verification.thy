159250bb25daa5068e77e66c9ee12371af94bcf4
SiyanWare <faydini065@gmail.com>
2026-05-30 17:06:11 +0300

    Update Verification.thy

--- formalverificationtest/Verification.thy
+++ formalverificationtest/Verification.thy
@@ -4,7 +4,20 @@ begin
 
 section "Formal Verification"
 
-local_setup "fn lthy => let val file_path = \"/var/home/siyanware/verysecureos/src/core.bin\"; val file_size = Int.fromLarge (OS.FileSys.fileSize file_path); val size_term = HOLogic.mk_number @{typ nat} file_size; val b = @{binding \"physical_core_size\"}; val mx = NoSyn; val declaration = ((b, mx), ((Binding.empty, []), size_term)); val ((_, (_, def_thm)), lthy') = Local_Theory.define declaration lthy; val _ = writeln (\"Target evaluation completed. File size: \" ^ Int.toString file_size ^ \" bytes.\"); in lthy' end"
+local_setup "fn lthy => let
+  (* Master_Directory üzerinden projenin bulunduğu konumu dinamik olarak alıyoruz *)
+  val base_path = Resources.master_directory (Proof_Context.theory_of lthy);
+  val file_path = Path.append base_path (Path.explode \"../src/core.bin\");
+  val platform_path = Path.implode (Path.expand file_path);
+
+  val file_size = Int.fromLarge (OS.FileSys.fileSize platform_path);
+  val size_term = HOLogic.mk_number @{typ nat} file_size;
+  val b = @{binding \"physical_core_size\"};
+  val mx = NoSyn;
+  val declaration = ((b, mx), ((Binding.empty, []), size_term));
+  val ((_, (_, def_thm)), lthy') = Local_Theory.define declaration lthy;
+  val _ = writeln (\"Target evaluation completed. File size: \" ^ Int.toString file_size ^ \" bytes.\");
+in lthy' end"
 
 definition mathematical_file_size_bound :: nat where
 "mathematical_file_size_bound = 0"
